dee970a68c2cd6c5379e0149bbdd772a6d5f6080
Pedro <pedro.phdois@gmail.com>
2018-08-24 02:45:00 -0300

    criado inserção e edição de usuário

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +12,17 @@ export class HomeComponent implements OnInit {
 
 	users: Array<User>;
 
-	constructor(private userService: UserService) { }
+	constructor(
+		private userService: UserService,
+		private router: Router
+	) { }
 
 	ngOnInit() {
 		this.userService.getUsers().subscribe(users => this.users = users);
+	}
+
+	editUser(user: User) {
+		this.router.navigate(['/add-user', { id: user.id, action: 'edit' }]);
 	}
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -23,7 +23,7 @@ export class UserService {
 					email: 'admin@email.com',
 					username: 'admin',
 					password: '0192023a7bbd73250516f069df18b500',
-					userlevel: 1
+					userlevel: '1'
 				},
 				{
 					id: 2,
@@ -31,7 +31,7 @@ export class UserService {
 					email: 'user@email.com',
 					username: 'user',
 					password: '6ad14ba9986e3615423dfca256d04e3f',
-					userlevel: 2
+					userlevel: '2'
 				}
 			]))
 		}
@@ -53,8 +53,89 @@ export class UserService {
 		return of(USERS);
 	}
 
+	getUser(id: number):Observable<User> {
+		let hasUser = USERS.filter(function(existUser) {
+			if (id == existUser.id) return existUser;
+		});
+		if (hasUser.length > 0) return of(hasUser[0]);
+		else return of(null);
+	}
+
 	setLoggedUser(user: User) {
 		UserService.userInfo = user;
+	}
+
+	addUser(user: User): Observable<Object> {
+		let ret = new Object();
+		let hasUser = USERS.filter(function(existUser) {
+			if (user.username == existUser.username) return existUser;
+		});
+		if (hasUser.length > 0) {
+			ret['error'] = true;
+			ret['msg'] = 'User already exists';
+		}
+		else {
+			// get new id
+			let newId = 0;
+			USERS.forEach(function(user) {
+				if (user.id > newId) newId = user.id;
+			});
+			newId++;
+
+			user.id = newId;
+			USERS.push(user);
+			let cookieAux = JSON.parse(this.cookieService.get('users'));
+			cookieAux.push({
+				id: user.id,
+				name: user.name,
+				email: user.email,
+				username: user.username,
+				password: user.password,
+				userlevel: user.userlevel
+			});
+			this.cookieService.set('users', JSON.stringify(cookieAux));
+			ret['error'] = false;
+			ret['msg'] = 'User successfully registered';
+		}
+		return of(ret);
+	}
+
+	updateUser(user: User, editedPassword: boolean) {
+		let ret = new Object();
+		let hasUser = USERS.filter(function(existUser) {
+			if (user.id == existUser.id) return existUser;
+		});
+		if (hasUser.length == 0) {
+			ret['error'] = true;
+			ret['msg'] = 'User not found';
+		}
+		else {
+			let hasAnotherUser = USERS.filter(function(existUser) {
+				if (user.id != existUser.id && user.username == existUser.username) return existUser;
+			});
+			
+			if (hasAnotherUser.length > 0) {
+				ret['error'] = true;
+				ret['msg'] = 'A user with this username already exists';
+			}
+			else {
+				USERS.forEach(function(_user) {
+					if (user.id == _user.id) {
+						_user.name = user.name;
+						_user.email = user.email;
+						_user.userlevel = user.userlevel;
+						if (editedPassword) {
+							_user.password = user.password
+						}
+					}
+				});
+
+				this.cookieService.set('users', JSON.stringify(USERS));
+				ret['error'] = false;
+				ret['msg'] = 'User successfully updated';
+			}
+		}
+		return of(ret);
 	}
 
 	logout() {
